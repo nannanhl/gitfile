08f479fc3cf1f41a045f9d5ac2ff62e44a47f743
nannanhl <nannanhl@aliyun.com>
2019-08-15 13:24:02 +0800

    第 1 次提交data.py 文件

---
+++ data1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 11 23:15:53 2019
+
+@author: admin
+"""
+from pandas import DataFrame, Series
+import pandas as pd
+import numpy as np
+import json
+path = 'b:/python3/Data/example.txt'
+records = [json.loads(line) for line in open(path)]
+frame = DataFrame(records)
+frame
